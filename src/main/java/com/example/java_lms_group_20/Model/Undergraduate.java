d2151e91a83abd9317dde0d2e8f15d3dbaab19cf
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2026-04-07 08:12:05 +0530

    user updated

---
+++ src/main/java/com/example/java_lms_group_20/Model/Undergraduate.java
@@ -0,0 +1,66 @@
+package com.example.java_lms_group_20.Model;
+
+public class Undergraduate extends User {
+
+    private String studentID;
+    private String degreeProgram;
+    private int level;
+    private double gpa;
+
+    public Undergraduate() {
+    }
+
+    public Undergraduate(int userID, String username, String password,
+                         String firstName, String lastName,
+                         String email, String contactNo, String profilePicture,
+                         String studentID, String degreeProgram, int level, double gpa) {
+
+        super(userID, username, password, firstName, lastName, email, contactNo, profilePicture);
+        this.studentID = studentID;
+        this.degreeProgram = degreeProgram;
+        this.level = level;
+        this.gpa = gpa;
+    }
+
+    public Undergraduate(int userID, String username, String password,
+                         String firstName, String lastName,
+                         String email, String contactNo,
+                         String studentID, String degreeProgram) {
+
+        super(userID, username, password, firstName, lastName, email, contactNo, null);
+        this.studentID = studentID;
+        this.degreeProgram = degreeProgram;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getDegreeProgram() {
+        return degreeProgram;
+    }
+
+    public void setDegreeProgram(String degreeProgram) {
+        this.degreeProgram = degreeProgram;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
